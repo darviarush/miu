bf9be25d0fa7ce0d4e19c484cdc387c97b905555
Yaroslav O. Kosmina <y.kosmina@cian.ru>
2017-12-20 17:59:57 +0300

    master save

--- miu/test.pl
+++ miu/test.pl
@@ -1,16 +1,22 @@
-use IPC::Open3;
+# use IPC::Open3;
 
-use POSIX ":sys_wait_h";
+# use POSIX ":sys_wait_h";
 
-my($wtr, $rdr, $err);
-use Symbol 'gensym'; $err = gensym;
-$pid = open3($wtr, $rdr, $err,
-		'/cygdrive/c/sbin/nodejs/node.exe', '.miu/10-miu.t.js');
+# my($wtr, $rdr, $err);
+# use Symbol 'gensym'; $err = gensym;
+# $pid = open3($wtr, $rdr, $err,
+		# '/cygdrive/c/sbin/nodejs/node.exe', '.miu/10-miu.t.js');
 		
 		
 		
-while(waitpid( $pid, WNOHANG ) > 0) {
-	
-}
+# waitpid( $pid, 0 );
+# print "done\n";
 
-print "done\n";
+use IPC::Open3::Simple;
+
+my $ipc = IPC::Open3::Simple->new(
+	out => sub { print "out: " . $_[0]; },
+	err => sub { print "err: " . $_[0]; },
+);
+
+$ipc->run('/cygdrive/c/sbin/nodejs/node.exe .miu/10-miu.t.js');
